7501fed09420f8b02e09b31f621bc84ccc6262c3
Jacob Baloul <jacob@stabilityfocus.com>
2015-06-01 17:36:03 -0400

    fixed spider and conf

--- urlspider.sh
+++ urlspider.sh
@@ -10,14 +10,19 @@ export SITE2CRAWL=$1
 export CRAWLTIME=$2
 export URLS="$3"
 
-# TODO: set in conf
-export OUTDIR='outdir'
-export WGETLOG="$OUTDIR/wgetlog.txt"
+# OVERRIDES set in conf
+#export OUTDIR='outdir'
+#export WGETLOG="$OUTDIR/wgetlog.txt"
 #--# export URLS="urls.txt"
 #--# export URLS="$OUTDIR/urls.txt"
-export URLS_ASSETS="$OUTDIR/urls_assets.txt"
-export URLS_ASSETS_TMP="$OUTDIR/urls_assets_tmp.txt"
-export URLS_ALL="$OUTDIR/urls_all.txt"
+#export URLS_ASSETS="$OUTDIR/urls_assets.txt"
+#export URLS_ASSETS_TMP="$OUTDIR/urls_assets_tmp.txt"
+#export URLS_ALL="$OUTDIR/urls_all.txt"
+
+
+# to avoid sed errors on non standard chars
+export LC_CTYPE=C 
+export LANG=C
 
 
 if test -z "$1"
@@ -40,32 +45,63 @@ then
 	exit 1
 fi
 
+################
+# first things first, load conf file
+if test -e $PWD/etc/webtest.conf
+then
+	source $PWD/etc/webtest.conf
+	if test $? -gt 0; then
+		echo "$0 ERROR: cannot load configuration file @ $PWD/etc/webtest.conf"
+		exit 1
+	fi
+else
+	echo "Conf file missing: $PWD/etc/webtest.conf"
+	exit 1
+fi
+################
+#
+# FUNCTIONS
+#
+################
+function check_error() {
+if test $? -gt 0
+then
+	echo "$0 exited with errors"
+	exit 1
+fi
+}
+################
+function calcURLs() {
+export CURRENT_URL_COUNT="$(cat ${WGETLOG} | grep -a "URL:" | awk '{print $3}' | sed -e 's/URL://g' | grep -v '^$' | sort -u | wc -l | sed -e 's/ //g')"
+#--# export URLS_REMAINING="$(echo "$CRAWLCOUNT-$CURRENT_URL_COUNT" |bc)"
+}
+################
 
+export SpiderLogo='
+ ___  ___  ________  ___               ________  ________  ___  ________  _______   ________     
+|\  \|\  \|\   __  \|\  \             |\   ____\|\   __  \|\  \|\   ___ \|\  ___ \ |\   __  \    
+\ \  \\\  \ \  \|\  \ \  \            \ \  \___|\ \  \|\  \ \  \ \  \_|\ \ \   __/|\ \  \|\  \   
+ \ \  \\\  \ \   _  _\ \  \            \ \_____  \ \   ____\ \  \ \  \ \\ \ \  \_|/_\ \   _  _\  
+  \ \  \\\  \ \  \\  \\ \  \____        \|____|\  \ \  \___|\ \  \ \  \_\\ \ \  \_|\ \ \  \\  \| 
+   \ \_______\ \__\\ _\\ \_______\        ____\_\  \ \__\    \ \__\ \_______\ \_______\ \__\\ _\ 
+    \|_______|\|__|\|__|\|_______|       |\_________\|__|     \|__|\|_______|\|_______|\|__|\|__|
+                                         \|_________|                                            
+'
 
+################
+#
+# MAIN
+#
+
+# capture start time
+export CRAWLSTART="$(date)"
+export CRAWLEND="$(date -j -v +${CRAWLTIME}S)"
 
 mkdir -p $OUTDIR
+check_error
 
 # crawl site and find all urls
-wget --spider --recursive --no-verbose --output-file=${WGETLOG} ${SITE2CRAWL} &
-
-echo "
-	Crawl Started: $(date) 
-
-	crawling for $CRAWLTIME seconds.
-
-	Estimated End Time: $(date -j -v +${CRAWLTIME}S)
-
-	For progress, run the following command:
-	  # watch -d 'wc -l $OUTDIR/* ; echo ; ls -ltr $OUTDIR'
-
-
-crawling site, please wait...
-
-"
-
-sleep $CRAWLTIME
-kill %1
-
+#
 # wget --spider --recursive --output-file=${WGETLOG} -O - ${URL}
 
 #wget -q ${URL} -O - | \
@@ -78,18 +114,55 @@ kill %1
 #	grep -i -o '<a[^>]\+href[ ]*=[ \t]*"\(ht\|f\)tps\?:[^"]\+"' |  \
 #	sed -e 's/^.*"\([^"]\+\)".*$/\1/g' -e 's/.*href="//' -e 's/".*//' | \ 
 #	grep '^[a-zA-Z].*'
+wget --spider --recursive --no-verbose --output-file=${WGETLOG} ${SITE2CRAWL} &
+
+# give the craweler some time...
+sleep $CRAWLTIME &
+
+echo "
+loading spider...
+
+	$SpiderLogo                                                                                                
+"
+
+while sleep 5
+do 
+	calcURLs
+	clear
+	echo "
+
+
+	$SpiderLogo
+
+
+	Crawl Started: $CRAWLSTART 
+	Estimated End Time: $CRAWLEND
+
+	Crawling for $CRAWLTIME seconds...
+
+		->> $CURRENT_URL_COUNT URLs found
+
+	spider is crawling site, please wait...
+	"
+done
+
+wait %2 # wait for the crawl time
+kill %1 # kill the crawler
+check_error
 
 # extract URLs from wget log
 cat ${WGETLOG} | grep -a "URL:" | awk '{print $3}' | sed -e 's/URL://g' | grep -v '^$' | sort -u > ${URLS}
-
+check_error
 
 # find all external http calls and assets in source, this will take time depending on the amount of URLs
-echo "...
+echo "
+...
 finding all external http calls and assets in source, 
 this will take time depending on the amount of URLs
 
 please wait..."
 cat /dev/null > $URLS_ASSETS_TMP
+check_error
 cat ${URLS} | while read url
 do
 	curl -s $url | grep href | sed -e 's/.*href="//' -e 's/".*//' | grep '^[a-zA-Z].*' | sort -u | grep "http" >> $URLS_ASSETS_TMP
@@ -97,10 +170,12 @@ done
 
 # extract unique URLs from assets and external URLs
 cat $URLS_ASSETS_TMP | sort -u > $URLS_ASSETS
+check_error
 
 
 # combine all unique URLs into one file
 cat $URLS $URLS_ASSETS | sort -u > $URLS_ALL
+check_error
 
 echo "
 
@@ -118,6 +193,8 @@ $(wc -l $URLS_ALL)
 
 ======================
 "
+
+check_error
 
 exit 0
 
